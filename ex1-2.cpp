b3f5f24aa772f489bc67642b2c69714ca8394c2d
wangphoebe109 <138782811+wangphoebe109@users.noreply.github.com>
2023-07-06 21:16:33 +0800

    Create ex1-2.cpp

---
+++ ex1-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void) {
+    int i = 1, a,b;
+    float f = 66.4;
+    char  c = 'A';
+    a = 20 ;
+    b = 50 ;
+    printf("hello Selena");
+    printf("my height is %d m %f cm \n", i, f);
+    printf("in my last name , its first letter is %c \n", c);
+    printf(" a + b = %d \n", a+b ); // + , - , * , /
+}
