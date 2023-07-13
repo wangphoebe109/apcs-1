16d3d9ab9cc9b402eacc6ece459faf2d0476385b
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2023-07-13 17:03:11 +0800

    Create a-4.cpp

---
+++ a-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int x,y;
+    int bool_value;
+    x = 3 ; y = 4;
+    // 1:true ; 0:false
+    bool_value = ( x > y );
+    printf(" x > y ==> %d \n ",bool_value);
+    bool_value = ( x < y );
+    printf(" x < y ==> %d \n ",bool_value);
+    bool_value = (" x < 5 && x < 10 ==> %d \n ",bool_value);
+}
