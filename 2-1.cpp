5c0c80e6d5c74929955520c3e73bda91ca47eaf4
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2023-07-13 21:08:44 +0800

    Create 2-1.cpp

---
+++ 2-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 30 , b = 20;
+    int c;
+    if(a < b){
+        printf(" a < b && a > 5 \n");
+    }else if(a == b){
+        printf(" a == b \n");
+    }else{
+        printf(" a> b \n");
+    }
+    return 0;
+}
