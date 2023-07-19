a1e445a45046b458f4bee3283ba8ee288b3fde71
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2023-07-19 21:02:27 +0800

    Update t2-4.cpp

--- t2-4.cpp
+++ t2-4.cpp
@@ -5,7 +5,6 @@ int main()
     printf("your born year and month: \n");
     scanf("%d", &y );
     scanf("%d", &m);
-    //if ( m<1 || m>12){
     if( y<1 ){
         printf("false!!!");
     }else if( m<1 || m>12 ){
@@ -20,14 +19,5 @@ int main()
     else {
         printf("There are 30 days in this month.");
     }
-    /*
-    else if ( m>8 && m%2==0){
-        printf("There are 30 days in this month.");
-    }
-    if(y%4==0 && y%400!=0) {
-        printf("Yes, leap year");
-    } else {
-        printf("NO, Not leap year");
-    }*/
     return 0;
 }
