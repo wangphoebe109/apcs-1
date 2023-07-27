9c1590a1b5e920f1b5a8e8d09aad79b74e383910
Cloud Girl <138782811+wangphoebe109@users.noreply.github.com>
2023-07-27 20:10:50 +0800

    Create t-21.cpp

---
+++ t-21.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int addNumber(int h, int m);
+int main()
+{
+    int a,b,min;
+    printf("enter two numer:");
+    scanf("%d %d",&a,&b);
+    min = addNumber(a,b);
+    printf("min = %d",min);
+    return 0;
+}
+    int addNumber(int h,int m){
+    int result;
+    result = h*60+m;
+    return result;
+}
